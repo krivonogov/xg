1d1a22485fe5310306237a7bb8b477d8b362b6fe
krivonogov <andrey.krivonogov@tinyclues.com>
2017-03-31 09:37:02 +0200

    Update xg_calculator.py
    update XGCalculator

--- xg_calculator.py
+++ xg_calculator.py
@@ -3,11 +3,9 @@ from abc import ABCMeta, abstractmethod
 import numpy as np
 from scipy.special import expit
 
-__author__ = 'Andrey Krivonogov <krivonogov.andrey@gmail.com>'
+from predicteo.utils.math_utils import REG, YARD
 
-YARD = 0.9144
-REG = 10e-7
-CUTBACK_THRESH_IN_METERS = 4
+__author__ = 'Andrey Krivonogov <krivonogov.andrey@gmail.com>'
 
 
 def check_shot_in_pitch(x, y):
@@ -76,41 +74,6 @@ def log_reg_dot(beta_dict, shot_params, verbose=False):
     return expit(res * player_adjustment)
 
 
-def if_assist_across_face(x, y, assist_x, assist_y):
-    y_box = 5.5
-    x_min_box = 30.34 - 5.5
-    x_max_box = 37.66 + 5.5
-
-    if (x_min_box < x < x_max_box and y < y_box) or (x_min_box < assist_x < x_max_box and assist_y < y_box):
-        # one of the points is in the box
-        return True
-
-    if y >= y_box and assist_y >= y_box:
-        # both points are above y = y_box
-        return False
-
-    if ((x - x_min_box) * (assist_x - x_min_box) >= 0) and ((x - x_max_box) * (assist_x - x_max_box) >= 0):
-        # both points are in the same coridor
-        return False
-
-    # other cases are treated using
-    # http://stackoverflow.com/questions/4977491/determining-if-two-line-segments-intersect/4977569#4977569
-    # we check whether passing line intersects with one of the lines of box
-    for x0, y0, x1, y1 in [(x_min_box, 0, x_min_box, y_box),  # x = x_min_box
-                           (x_min_box, y_box, x_max_box, y_box),  # y = y_box
-                           (x_max_box, 0, x_max_box, y_box)]:  # x = x_max_box
-        det = (x1 - x0) * (y - assist_y) - (x - assist_x) * (y1 - y0)
-        if abs(det) < 1e-6:
-            continue
-        s = (assist_x - x0) * (y - assist_y) - (assist_y - y0) * (x - assist_x)
-        s /= det
-        t = (assist_x - x0) * (y1 - y0) - (assist_y - y0) * (x1 - x0)
-        t /= det
-        if 0 < s < 1 and 0 < t < 1:
-            return True
-    return False
-
-
 class XGCalculator(object):
     __metaclass__ = ABCMeta
 
@@ -125,10 +88,10 @@ class XGCalculator(object):
         self.goal_width = self.right_post[0] - self.left_post[0]
 
     @abstractmethod
-    def eval(self, x, y, **kwargs):
+    def eval(self, **kwargs):
         """
-        :param x: meters from right (looking from the center of the pitch) touchline
-        :param y: meters from goal line
+        :param kwargs: should contain x meters from right (looking from the center of the pitch) touchline and
+                                      y meters from goal line
         :return: expected goal result
         """
         pass
@@ -136,6 +99,9 @@ class XGCalculator(object):
     def _relative_angle(self, x, y):
         """
         Angle from (x, y) to the nearest post divided by pi / 2 as described by Caley
+        >>> xg = CaleyXGCalculator()
+        >>> xg._relative_angle(x=44.948, y=1.392857)
+        0.12021884320255356
         """
         if self.left_post[0] <= x <= self.right_post[0]:
             return 1.
@@ -168,42 +134,6 @@ class XGCalculator(object):
         return y < 5.5 and (30.34 < x < 37.66 or (30.34 - 5.5 < x < 37.66 + 5.5 and self._relative_angle(x, y) > 0.75))
 
 
-class SimpleXGCalculator(XGCalculator):
-    """
-    XG model with beta coefficients used in http://torvaney.github.io/xG.html
-    """
-    def __init__(self):
-        super(SimpleXGCalculator, self).__init__()
-        # order of features:
-        # 0     intercept
-        # 1:4   angle, goal_dist, header
-        # 4:7   angle * goal_dist, angle * header, goal_dist * header
-        # 7     angle * goal_dist * header
-        self._beta = {'1': -1.745598, 'angle': 1.338737, 'distance': -0.110384, 'header': 0.646730,
-                      ('angle', 'distance'): 0.168798, ('angle', 'header'): -0.424885,
-                      ('distance', 'header'): -0.134178, ('angle', 'distance', 'header'): -0.055093}
-
-    def eval(self, x, y, **kwargs):
-        """
-        Kwargs: header, verbose
-        >>> xg = SimpleXGCalculator()
-        >>> xg.eval(34, 11)
-        0.28761592428370542
-        >>> xg.eval(37, 20)
-        0.09166111179904099
-        """
-        check_shot_in_pitch(x, y)
-        header = int(kwargs.get('header', False))
-        verbose = kwargs.pop('verbose', False)
-
-        angle = self._angle_between_posts(x, y)
-
-        # distance to the centre of the goal
-        goal_dist = self._distance(x, y, self.goal_center)
-
-        return log_reg_dot(self._beta, {'angle': angle, 'distance': goal_dist, 'header': header}, verbose=verbose)
-
-
 class CaleyXGCalculator(XGCalculator):
     """
     XG model introduced by Caley in
@@ -216,7 +146,7 @@ class CaleyXGCalculator(XGCalculator):
     def _beta(dribble_goal, direct_free_kick, cross, header):
         if dribble_goal:
             # Shots Following a Dribble of the Keeper
-            return {'1': -0.61, 'distance': -0.09, 'inverse_distance': 7.4, 'angle': 1.04, 'big_change': 1.1,
+            return {'1': -0.61, 'distance': -0.09, 'inverse_distance': 7.4, 'angle': 1.04, 'big_chance': 1.1,
                     'following_error': 0.67, ('inverse_distance', 'inverse_angle'): -3.2}
         elif direct_free_kick:
             # Shots from Direct Free Kicks
@@ -225,8 +155,8 @@ class CaleyXGCalculator(XGCalculator):
         elif cross:
             if header:
                 # Headed Shots Assisted by Crosses
-                return {'1': -2.88, 'distance': -0.21, 'relative_angle': 2.13, 'inverse_assist_distance': 4.31,
-                        'assist_angle': 0.46, 'fastbreak': 0.2, 'counterattack': 0.11, 'set_play': 0.12,
+                return {'1': -2.88, 'distance': -0.21, 'angle': 2.13, 'inverse_assist_distance': 4.31,
+                        'assist_angle': 0.46, 'fastbreak': 0.2, 'counterattack': 0.11, 'set_piece': 0.12,
                         'corner': -0.24, 'otherbodypart': -0.18, 'big_chance': 1.2, 'following_error': 1.1,
                         'EPL': 0.18, 'LaLiga': 0.15}
             else:
@@ -237,7 +167,7 @@ class CaleyXGCalculator(XGCalculator):
                         'following_error': 1.1, 'EPL': -0.2}
         elif header:
             # Headed Shots Not Assisted by Crosses
-            return {'1': -3.85, 'distance': -0.1, 'inverse_distance': 2.56, 'relative_angle': 1.94,
+            return {'1': -3.85, 'distance': -0.1, 'inverse_distance': 2.56, 'angle': 1.94,
                     'throughball_assist': 0.51, 'fastbreak': 0.44, 'counterattack': 0.26, 'rebound': 0.7,
                     'established_possession': 0.44, 'otherbodypart': 1.14, 'big_chance': 1.3, 'following_error': 1.1,
                     'EPL': -0.29, 'LaLiga': -0.24, 'SerieA': -0.26}
@@ -245,56 +175,73 @@ class CaleyXGCalculator(XGCalculator):
             # Regular Shots
             # XXX: we use EPL and LaLiga coefs equal to 0.05 instead of -0.1 and -0.09 in order not to underestimate
             # results in these leagues
-            return {'1': -3.19, 'distance': -0.095, 'inverse_distance': 3.18, 'relative_angle': 1.88,
+            return {'1': -3.19, 'distance': -0.095, 'inverse_distance': 3.18, 'angle': 1.88,
                     'inverse_angle': 0.24, ('inverse_distance', 'inverse_angle'): -2.09, 'throughball_assist': 0.45,
                     'throughball_2nd_assist': 0.64, 'assist_across_face': 0.31, 'cutback_assist': -0.15,
                     'inverse_assist_distance': 2.18, 'assist_angle': 0.12, 'fastbreak': 0.23, 'counterattack': 0.18,
                     'established_possession': 0.09, 'following_corner': -0.18, 'big_chance': 1.2,
                     'following_error': 1.1, 'following_dribble': 0.39, 'dribble_distance': 0.14, 'rebound': 0.37,
-                    'game_state': 0.03, 'Bundesliga': 0.07, 'EPL': 0.05, 'LaLiga': 0.05, 'SerieA': -0.07}
+                    'game_state_sgn': 0.03, 'Bundesliga': 0.07, 'EPL': 0.05, 'LaLiga': 0.05, 'SerieA': -0.07}
 
-    def eval(self, x, y, **kwargs):
+    def eval(self, **kwargs):
         """
         Common kwargs:  'header', 'dribble_goalkeeper', 'cross', 'direct_free_kick', 'big_chance', 'following_error'
         League specific True/False kwargs: 'Bundesliga', 'EPL', 'LaLiga', 'SerieA'
         Assist kwargs for crosses and regular shots: 'assist_point'
-        Only header related kwargs: 'otherbodypart', 'set_play'
+        Only header related kwargs: 'otherbodypart', 'set_piece'
         Only cross related kwargs: 'corner'
         Speed related kwargs: 'fastbreak', 'counterattack'
         Other kwargs:   'throughball_assist', 'throughball_2nd_assist', 'cutback_assist',
                         'rebound', 'established_possession', 'game_state',
                         'following_corner', 'following_dribble', 'start_run_point'
         >>> xg = CaleyXGCalculator()
-        >>> xg.eval(34, 11, big_chance=True, throughball_assist=True, EPL=True, assist_point=(34, 18))
-        0.45227739927975757
-        >>> xg.eval(37, 20, direct_free_kick=True, LaLiga=True)
+        >>> xg.eval(x=34, y=11, big_chance=True, throughball_assist=True, EPL=True, assist_x=34, assist_y=18)
+        0.45227739927975769
+        >>> xg.eval(x=37, y=20, direct_free_kick=True, LaLiga=True)
         0.10529256313817077
-        >>> xg.eval(34, 11)
+        >>> xg.eval(x=34, y=11)
         0.10695610254229779
-        >>> xg.eval(34, 11, assist_point=(37, 25))
+        >>> xg.eval(x=34, y=11, assist_x=37, assist_y=25)
         0.12752188916686916
-        >>> xg.eval(32, 0.1)
+        >>> xg.eval(x=32, y=0.1)
         0.70504634688503887
-        >>> xg.eval(30, 7)
+        >>> xg.eval(x=30, y=7)
         0.15264470289986498
-        >>> xg.eval(28.7, 0.1, header=True)
-        0.019590797586891927
-        >>> xg.eval(24.4, 0.5, cross=True)
+        >>> xg.eval(x=24.4, y=0.5, cross=True)
         0.027634985220878282
-        >>> xg.eval(2, 2) < 1e-5
+        >>> xg.eval(x=28.7, y=0.1, header=True) < 1e-5
         True
-        >>> xg.eval(42, 1) < 1e-5
+        >>> xg.eval(x=2, y=2) < 1e-5
         True
-        >>> xg.eval(2, 1, header=True) < 1e-3
+        >>> xg.eval(x=42, y=1) < 1e-5
         True
+        >>> xg.eval(x=2, y=1, header=True) < 1e-3
+        True
+        >>> xg.eval(x=44.948, y=1.392857)
+        2.9305605013432191e-05
+        >>> xg.eval(x=33.660, y=11.785714)
+        0.098866030922461545
         """
+        x = kwargs['x']
+        y = kwargs['y']
         check_shot_in_pitch(x, y)
         if not kwargs.get('dribble_goalkeeper', False) and y < 5.5 and 30.34 < x < 37.66:
             y = max(0.5, y)
-        start_run_point = kwargs.pop('start_run_point', None)
-        assist_point = kwargs.pop('assist_point', None)
+        start_run_x, start_run_y = kwargs.pop('start_run_x', None), kwargs.pop('start_run_y', None)
+        assist_x, assist_y = kwargs.pop('assist_x', None), kwargs.pop('assist_y', None)
         verbose = kwargs.pop('verbose', False)
-        cross_or_header = kwargs.get('cross', False) or kwargs.get('header', False)
+
+        body_part = kwargs.pop('body_part', 'RightFoot')
+        kwargs['header'] = body_part == 'header' or body_part == 'otherbodypart'
+        kwargs['otherbodypart'] = body_part == 'otherbodypart'
+
+        kwargs['game_state_sgn'] = np.sign(kwargs.get('game_state', 0))
+        kwargs[kwargs.pop('league', None)] = True
+        kwargs[kwargs.pop('pass_type', None)] = True
+        kwargs[kwargs.pop('attack_type', 'open_play')] = True
+        kwargs['counterattack'] = kwargs.get('counterattack', False) or kwargs.get('fastbreak', False)
+
+        cross_or_header = kwargs.get('cross', False) or kwargs['header']
 
         shot_params = {k: (float(v) if not isinstance(v, tuple) else v) for k, v in kwargs.iteritems()}
         if cross_or_header:
@@ -302,29 +249,20 @@ class CaleyXGCalculator(XGCalculator):
         else:
             shot_params['distance'] = y / YARD
 
-        shot_params['relative_angle'] = self._relative_angle(x, y)
-        shot_params['angle'] = shot_params['relative_angle']
+        shot_params['angle'] = self._relative_angle(x, y)
 
-        if start_run_point is not None:
-            if cross_or_header:
-                start_distance = self._distance(start_run_point[0], start_run_point[1], self.goal_center, yards=True)
-            else:
-                start_distance = start_run_point[1] / YARD
-
+        if start_run_x is not None and start_run_y is not None and not cross_or_header:
+            start_distance = max(0.5, start_run_y) / YARD
             shot_params['dribble_distance'] = (start_distance - shot_params['distance']) / start_distance
-        else:
-            shot_params['dribble_distance'] = 0.
 
-        if assist_point is not None:
-            assist_x, assist_y = assist_point
+        if assist_x is not None and assist_y is not None:
+            assist_x = float(assist_x)
+            assist_y = float(assist_y)
             shot_params['assist_distance'] = self._distance(assist_x, assist_y, self.goal_center, yards=True)
             shot_params['assist_angle'] = self._relative_angle(assist_x, assist_y)
-            shot_params['assist_across_face'] = float(if_assist_across_face(x, y, assist_x, assist_y))
-            shot_params['cutback_assist'] = float((y - assist_y) > CUTBACK_THRESH_IN_METERS)
 
         return log_reg_dot(self._beta(kwargs.get('dribble_goalkeeper', False),
                                       kwargs.get('direct_free_kick', False),
                                       kwargs.get('cross', False),
                                       kwargs.get('header', False)),
                            shot_params, verbose=verbose)
-
